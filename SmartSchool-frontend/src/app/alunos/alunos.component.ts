fecb7aaf6bc5867bf1e3e2d7b4acb98adac9f617
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-15 02:46:54 -0300

    Tela de interação com aluno
    Criado tela que realiza a interação com o Aluno.

--- SmartSchool-frontend/src/app/alunos/alunos.component.ts
+++ SmartSchool-frontend/src/app/alunos/alunos.component.ts
@@ -9,23 +9,33 @@ import { MatPaginator } from '@angular/material/paginator';
   styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
-  public titulo:string = 'Lista de Alunos';
+  public titulo:string = 'Área do Aluno';
+  public alunoSelecionado?: string;
 
   public alunos: Aluno [] = [
-    {Id: 1, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 2, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 3, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 4, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 5, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 6, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 7, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
-    {Id: 8, nome: "Júlio César", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 1, nome: "Júlio César1", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 2, nome: "Júlio César2", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 3, nome: "Júlio César3", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 4, nome: "Júlio César4", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 5, nome: "Júlio César5", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 6, nome: "Júlio César6", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 7, nome: "Júlio César7", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
+    {Id: 8, nome: "Júlio César8", sobrenome: "Mesquita Camilo", telefone: "85 996816053", email: "juliocesarmcamilo@gmail.com" },
   ];
 
   displayedColumns = ['Id', 'nome', 'sobrenome', 'telefone', 'email', 'acao'];
 
+  AlunoSelecionado(aluno: Aluno){
+    this.alunoSelecionado = aluno.nome;
+  };
+  AlunoDeselecionado(){
+    this.alunoSelecionado = '';
+  }
+
+  // teste de paginação | voltar depois
   dataAluno = new MatTableDataSource<Aluno>(this.alunos);
   @ViewChild(MatPaginator) paginator!:MatPaginator;
+  // Fim do teste de paginação | Voltar depois
   constructor() { }
 
   ngOnInit() {
